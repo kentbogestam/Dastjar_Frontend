321ba22c002cb6a41973a255ef3b5ed5ec7f33cb
divyaaxovel <divya.ravish@axovel.in>
2018-01-15 18:33:27 +0530

    for footer fixed

--- resources/views/menulist/index.blade.php
+++ resources/views/menulist/index.blade.php
@@ -116,7 +116,7 @@
 
 
 
-		<div data-role="footer" class="footer" data-position="fixed">
+		<div data-role="footer" class="footer" data-tap-toggle="false" data-position="fixed">
 			<div class="ui-grid-c inner-footer center">
 			<div class="ui-block-a"><a href="{{ url('eat-now') }}" class="ui-shadow ui-btn ui-corner-all icon-img ui-btn-inline" data-ajax="false">
 				<div class="img-container">
