90917c3631f64c77ba34852e1cc6241ffd8bfc48
divyaaxovel <divya.ravish@axovel.in>
2018-01-23 12:42:46 +0530

    for slider and range

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -7,7 +7,7 @@
 				<ul> 
 			<li><a href="{{url('eat-now')}}" data-ajax="false" class="text-left"><img src="{{asset('images/icons/backarrow.png')}}" width="11px"></a></li>
 			 <li><a data-ajax="false" class="ui-btn-active">Setting</a></li>
-			  <li class="done-btn">  <input type="button" value="Done" id="dataSave"/></li> </ul> </div><!-- /navbar -->
+			  <li class="done-btn" id="dataSave">  <input type="button" value="Done" /></li> </ul> </div><!-- /navbar -->
 		</div>
 	</div>
 
@@ -49,12 +49,12 @@
 						@endif </p></h2>
 						
 						    <fieldset data-role="controlgroup">
-						        <input type="radio" name="radio-choice-v-2" id="radio-choice-v-2a" value="ENG" checked="checked">
+						        <input type="radio" name="radio-choice-v-2" id="radio-choice-v-2a" value="ENG" @if(Auth::user()->language == 'ENG') checked="checked" @else checked="checked" @endif>
 						        <label for="radio-choice-v-2a">English</label>
-						        <input type="radio" name="radio-choice-v-2" id="radio-choice-v-2b" value="SWE">
+						        <input type="radio" name="radio-choice-v-2" id="radio-choice-v-2b" value="SWE" @if(Auth::user()->language == 'SWE') checked="checked" @endif>
 						        <label for="radio-choice-v-2b">Swedish</label>
-						        <input type="radio" name="radio-choice-v-2" id="radio-choice-v-2c" value="GER">
-						        <label for="radio-choice-v-2c">German</label>
+						        <input type="radio" name="radio-choice-v-2" id="radio-choice-v-2c" value="GER" >
+						        <label for="radio-choice-v-2c" @if(Auth::user()->language == 'GER') checked="checked" @endif>German</label>
 						    </fieldset>
 						
 					</li>	
@@ -62,7 +62,7 @@
 					<li data-role="collapsible" class="range-sec"><h2  class="ui-btn ui-btn-icon-right ui-icon-carat-r">Range<p class="ui-li-aside">{{Auth::user()->range}} Km</p></h2>
 					<p>
 						<div data-role="rangeslider">
-						    <input type="range" name="range-1b" id="range-1b" min="3" max="10" value="">
+						    <input type="range" name="range-1b" id="range-1b" min="3" max="10" value="{{Auth::user()->range}}">
 						</div>
 					</p>
 					</li> 
