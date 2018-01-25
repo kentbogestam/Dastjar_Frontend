3468b2c1143647827eafe64a202024ba791fd2fb
divyaaxovel <divya.ravish@axovel.in>
2018-01-25 16:31:36 +0530

    for view changes in kitchen and menu list

--- public/js/main.js
+++ public/js/main.js
@@ -77,7 +77,7 @@ function decrementValue(id)
 {
     var value = parseInt(document.getElementById(id).value, 10);
     value = isNaN(value) ? 0 : value;
-    if(value>1){
+    if(value>0){
         value--;
             document.getElementById(id).value = value;
     }
