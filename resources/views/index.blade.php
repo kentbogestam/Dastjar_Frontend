8c4d70459610e96ed0496747edc91f8dd217b427
divyaaxovel <divya.ravish@axovel.in>
2018-02-02 19:56:49 +0530

    for image overlap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -202,6 +202,7 @@ var totalCount = 0;
 	          	liItem += "<li class='ui-li-has-count ui-li-has-thumb ui-first-child'>";
 	          	liItem += "<a class = 'ui-btn ui-btn-icon-right ui-icon-carat-r' href="+url+"/"+list[i]['store_id']+" data-ajax='false'>";
 	          	liItem += "<img src="+"'"+list[i]["store_image"]+"'"+">";
+	          	liItem += "<h2>"+temp[i]["store_name"]+"</h2>";
 	          	liItem += "<p>";
 	          	
 	          	for (var j=0;j<list[i]["products"].length;j++){
