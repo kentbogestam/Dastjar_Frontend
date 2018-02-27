4f9efc6f26f178d63f5b624a372cb880b80a2bb8
divyaaxovel <divya.ravish@axovel.in>
2018-02-27 16:28:54 +0530

    map root

--- resources/views/map/single_res_map.blade.php
+++ resources/views/map/single_res_map.blade.php
@@ -75,7 +75,10 @@
 
 @section('footer-script')
 <!-- <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false&libraries=places"></script> -->
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyByLiizP2XW9JUAiD92x57u7lFvU3pS630"></script>
+<!-- <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyByLiizP2XW9JUAiD92x57u7lFvU3pS630"></script> -->
+<script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyByLiizP2XW9JUAiD92x57u7lFvU3pS630&callback=initMap">
+    </script>
 
 	<script type="text/javascript">
 		var headerHeight = $( '.header' ).height();
@@ -86,80 +89,109 @@
 		/*alert( height);*/
 		$( '.content' ).height( height );
 
+		function initMap() {
+			var directionsService = new google.maps.DirectionsService;
+			var directionsDisplay = new google.maps.DirectionsRenderer;
+			var markers = {!! $latLngList !!};
+			for( i = 0; i < markers.length; i++ ) {
+				var userLat = markers[0][0];
+				var userLong = markers[0][1];
+			}
+			var map = new google.maps.Map(document.getElementById('map'), {
+			  zoom: 7,
+			  center: {lat: userLat, lng: userLong}
+			});
+			directionsDisplay.setMap(map);
 
-		function initialize() {
-	    var map;
-	    var bounds = new google.maps.LatLngBounds();
-	    var mapOptions = {
-	        mapTypeId: 'roadmap'
-	    };
+
+			  calculateAndDisplayRoute(directionsService, directionsDisplay);
+
+			}
+
+			function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+			var markers = {!! $latLngList !!};
+			for( i = 0; i < markers.length; i++ ) {
+				var userLat = markers[0][0];
+				var userLong = markers[0][1];
+				var resLat = markers[1][0];
+				var resLongt = markers[1][1];
+			}
+			directionsService.route({
+			  origin: {lat: userLat, lng: userLong},
+			  destination: {lat: resLat, lng: resLongt},
+			  travelMode: 'DRIVING'
+			}, function(response, status) {
+			  if (status === 'OK') {
+			    directionsDisplay.setDirections(response);
+			  } else {
+			    window.alert('Directions request failed due to ' + status);
+			  }
+			});
+		}
+
+
+	// 	function initialize() {
+	//     var map;
+	//     var bounds = new google.maps.LatLngBounds();
+	//     var mapOptions = {
+	//         mapTypeId: 'roadmap'
+	//     };
 	                    
-	    // Display a map on the page
-	    map = new google.maps.Map(document.getElementById("map"), mapOptions);
-	    map.setTilt(45);
+	//     // Display a map on the page
+	//     map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	//     map.setTilt(45);
 	
-    	var markers = {!! $latLngList !!};
-	                        
-	    // Info Window Content
-	    /*var infoWindowContent = [
-	        ['<div class="info_content">' +
-	        '<h3>London Eye</h3>' +
-	        '<p>The London Eye is a giant Ferris wheel situated on the banks of the River Thames. The entire structure is 135 metres (443 ft) tall and the wheel has a diameter of 120 metres (394 ft).</p>' +        '</div>'],
-	        ['<div class="info_content">' +
-	        '<h3>Palace of Westminster</h3>' +
-	        '<p>The Palace of Westminster is the meeting place of the House of Commons and the House of Lords, the two houses of the Parliament of the United Kingdom. Commonly known as the Houses of Parliament after its tenants.</p>' +
-	        '</div>']
-	    ];*/
-	        
-	    // Display multiple markers on a map
-	    /*var infoWindow = new google.maps.InfoWindow(), marker, i;*/
-	    
-	    // Loop through our array of markers & place each one on the map  
-	    for( i = 0; i < markers.length; i++ ) {
-	        if( i == 0){
-		        var position = new google.maps.LatLng(markers[i][0], markers[i][1]);
-		        bounds.extend(position);
-		        marker = new google.maps.Marker({
-		            position: position,
-		            map: map,
-		            icon: {
-                    url:"{{ asset('images/blue-pin.png') }}",
-                    size: new google.maps.Size(71, 95),
-                    scaledSize: new google.maps.Size(35, 42),
-                    origin: new google.maps.Point(0, 0),
-                    anchor: new google.maps.Point(17, 36)
-                },
-                anchorPoint: new google.maps.Point(0, -29)
-		        });
-	    	}else{
-		        var position = new google.maps.LatLng(markers[i][0], markers[i][1]);
-		        bounds.extend(position);
-		        marker = new google.maps.Marker({
-		            position: position,
-		            map: map
-		        });
-	    	}
-	        
-	        // Allow each marker to have an info window    
-	        /*google.maps.event.addListener(marker, 'click', (function(marker, i) {
-	            return function() {
-	                infoWindow.setContent(infoWindowContent[i][0]);
-	                infoWindow.open(map, marker);
-	            }
-	        })(marker, i));*/
+ //    	var markers = {!! $latLngList !!};
+	//     for( i = 0; i < markers.length; i++ ) {
 
-	        // Automatically center the map fitting all markers on the screen
-	        map.fitBounds(bounds);
-	    }
+	//     console.log('userLat'+markers[0][0]); 
+	//     console.log('userLong'+markers[0][1]); 
+	//     console.log('resLat'+markers[1][0]); 
+	//     console.log('resLongt'+markers[1][1]); 
+	//         if( i == 0){
+	// 	        var position = new google.maps.LatLng(markers[i][0], markers[i][1]);
+	// 	        bounds.extend(position);
+	// 	        marker = new google.maps.Marker({
+	// 	            position: position,
+	// 	            map: map,
+	// 	            icon: {
+ //                    url:"{{ asset('images/blue-pin.png') }}",
+ //                    size: new google.maps.Size(71, 95),
+ //                    scaledSize: new google.maps.Size(35, 42),
+ //                    origin: new google.maps.Point(0, 0),
+ //                    anchor: new google.maps.Point(17, 36)
+ //                },
+ //                anchorPoint: new google.maps.Point(0, -29)
+	// 	        });
+	//     	}else{
+	// 	        var position = new google.maps.LatLng(markers[i][0], markers[i][1]);
+	// 	        bounds.extend(position);
+	// 	        marker = new google.maps.Marker({
+	// 	            position: position,
+	// 	            map: map
+	// 	        });
+	//     	}
+	        
+	//         // Allow each marker to have an info window    
+	//         /*google.maps.event.addListener(marker, 'click', (function(marker, i) {
+	//             return function() {
+	//                 infoWindow.setContent(infoWindowContent[i][0]);
+	//                 infoWindow.open(map, marker);
+	//             }
+	//         })(marker, i));*/
 
-	    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
-	    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-	        this.setZoom(17);
-	        google.maps.event.removeListener(boundsListener);
-	    });
+	//         // Automatically center the map fitting all markers on the screen
+	//         map.fitBounds(bounds);
+	//     }
+
+	//     // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+	//     var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+	//         this.setZoom(17);
+	//         google.maps.event.removeListener(boundsListener);
+	//     });
 	    
-	}
-		google.maps.event.addDomListener(window, 'load', initialize);
+	// }
+		// google.maps.event.addDomListener(window, 'load', initialize);
 
 
 		$(".ordersec").click(function(){
