b23f3b41aa40c875af2ca08349e41ed6b86e9342
divyaaxovel <divya.ravish@axovel.in>
2018-04-12 19:24:59 +0530

    app check out

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -11,8 +11,10 @@
  	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css?family=Aclonica" rel="stylesheet">
 
+	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="mobile-web-app-capable" content="yes">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<meta name="apple-mobile-web-app-title" content="Add to Home">
 	<link rel="apple-touch-icon-precomposed" href="{{asset('addToHomeIphoneImage/icon-152x152.png')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('addToHomeIphoneCss/addtohomescreen.css')}}">
 	@yield('styles')
