c2f494876b87bd5faabdf43b8df6cba4f5a2316e
divyaaxovel <divya.ravish@axovel.in>
2018-04-22 19:07:29 +0530

    iphone version

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -540,7 +540,56 @@
 	}
 	var count = getCookie("iphonePopupcount") + getCookie("iphonePopupcountIncrease");
 	console.log('count='+count);
-	if(getCookie("browser") == 'Safari' && count == 1){
+
+
+
+	var IphoneVersion;
+    var deviceDetection = function () { 
+    var osVersion, 
+    device, 
+    deviceType, 
+    userAgent, 
+    isSmartphoneOrTablet; 
+
+    device = (navigator.userAgent).match(/Android|iPhone|iPad|iPod/i); 
+
+    if ( /Android/i.test(device) ) { 
+        if ( !/mobile/i.test(navigator.userAgent) ) { 
+            deviceType = 'tablet'; 
+        } else { 
+            deviceType = 'phone'; 
+        } 
+
+        osVersion = (navigator.userAgent).match(/Android\s+([\d\.]+)/i); 
+        osVersion = osVersion[0]; 
+        osVersion = osVersion.replace('Android ', ''); 
+
+    } else if ( /iPhone/i.test(device) ) { 
+        deviceType = 'phone'; 
+        osVersion = (navigator.userAgent).match(/OS\s+([\d\_]+)/i); 
+        osVersion = osVersion[0]; 
+        osVersion = osVersion.replace(/_/g, '.'); 
+        osVersion = osVersion.replace('OS ', ''); 
+
+    } else if ( /iPad/i.test(device) ) { 
+        deviceType = 'tablet'; 
+        osVersion = (navigator.userAgent).match(/OS\s+([\d\_]+)/i); 
+        osVersion = osVersion[0]; 
+        osVersion = osVersion.replace(/_/g, '.'); 
+        osVersion = osVersion.replace('OS ', ''); 
+    } 
+    isSmartphoneOrTablet = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent); 
+    userAgent = navigator.userAgent; 
+    IphoneVersion = osVersion;
+    return { 'isSmartphoneOrTablet': isSmartphoneOrTablet, 
+             'device': device, 
+             'osVersion': osVersion, 
+             'userAgent': userAgent, 
+             'deviceType': deviceType 
+            }; 
+    }();
+    console.log('osVersion'+IphoneVersion);
+	if(getCookie("browser") == 'Safari' && count == 1 && IphoneVersion != '11.3'){
 		console.log('iphonePopupcount='+getCookie("iphonePopupcount"));
 		document.cookie="iphonePopupcountIncrease=" + 2;
 		var ath = addToHomescreen({
