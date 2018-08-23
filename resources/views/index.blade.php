d9b6f660708ab9e8fc9bf145ee1d59702cf964a3
Mayank <mayank.pathak@imaginei.in>
2018-08-23 17:31:37 +0530

    test commit
    test commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -103,7 +103,7 @@
 			<div class="logo">
 				<div class="inner-logo">
 					<img src="{{asset('images/logo.png')}}">
-					@if(Auth::check())<span>{{ Auth::user()->name}}</span>@endif
+					@if(Auth::check())<span>1{{ Auth::user()->name}}</span>@endif
 				</div>
 			</div>
 			<a href="{{url('search-map-eatnow')}}" class="ui-btn-right map-btn user-link" data-ajax="false"><img src="{{asset('images/icons/map-icon.png')}}" width="30px"></a>
