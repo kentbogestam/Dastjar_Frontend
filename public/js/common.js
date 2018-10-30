b1f5d533d9d96269ee60267d062f5a86cb01bc2a
saurabhaxovel <saurabh.php007@gmail.com>
2018-10-30 12:14:13 +0530

    Added common.js

---
+++ public/js/common.js
@@ -0,0 +1,40 @@
+
+
+setInterval(function(){getCurrentCoordinates()},3000); // Check the position afer 20 min and reset the longitude and latitude
+
+function getCurrentCoordinates(){
+
+   navigator.geolocation.getCurrentPosition(function(position) {  
+
+	    document.cookie="latitude=" + position.coords.latitude;
+	    document.cookie="longitude=" + position.coords.longitude;
+
+
+	
+	},function(error){
+		if (typeof lat === "undefined") {
+		   // $('.login-inner-section a').attr('href','javascript:void(0)');
+		   // $('#login-popup').show();	    			
+		}else{
+		    // document.cookie="latitude=" + lat;
+		    // document.cookie="longitude=" + lng;			
+		} 			    
+	});
+
+}
+
+function getCookie(cname) {
+	    var name = cname + "=";
+	    var decodedCookie = decodeURIComponent(document.cookie);
+	    var ca = decodedCookie.split(';');
+	    for(var i = 0; i <ca.length; i++) {
+	        var c = ca[i];
+	        while (c.charAt(0) == ' ') {
+	            c = c.substring(1);
+	        }
+	        if (c.indexOf(name) == 0) {
+	            return c.substring(name.length, c.length);
+	        }
+	    }
+	    return "";
+}
