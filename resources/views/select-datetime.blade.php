4e02fd03f769e864bb4496c9c9334beab82ce5ca
saurabhaxovel <saurabh.php007@gmail.com>
2018-12-07 13:36:14 +0530

    removed footer code

--- resources/views/select-datetime.blade.php
+++ resources/views/select-datetime.blade.php
@@ -104,27 +104,7 @@ $(document).ready(function(){
 			</div>
 		</form>
 
-
-		<div data-role="footer" class="footer" data-position="fixed" data-tap-toggle="false">
-			<div class="ui-grid-c inner-footer center">
-				<div class="ui-block-a"><a href="{{ Session::get('route_url') }}" class="ui-shadow ui-btn ui-corner-all icon-img ui-btn-inline" data-ajax="false">
-					<div class="img-container">
-						<img src="{{asset('images/icons/select-store_01.png')}}">
-					</div>
-					<span>{{ __('messages.Restaurant') }}</span>
-				</a></div>
-				<div class="ui-block-b"><a href="#" class="ui-shadow ui-btn ui-corner-all icon-img ui-btn-inline" data-ajax="false">
-					<div class="img-container">
-						<img src="{{asset('images/icons/select-store_03.png')}}">
-					</div>
-					<span>{{ __('messages.Send') }}</span>
-				</a></div>
-				@include('orderQuantity')
-				<div class="ui-block-d"><a href="{{url('user-setting')}}" class="ui-shadow ui-btn ui-corner-all icon-img ui-btn-inline" data-ajax="false">
-					<div class="img-container"><img src="{{asset('images/icons/select-store_07.png')}}"></div>
-				</a></div>
-			</div>
-		</div>
+		@include('includes.fixedfooter')
 	</div>
 
 @endsection
