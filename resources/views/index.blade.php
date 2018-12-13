afc4fbaad5113b3c9eaed9c1ead87fd886115899
saurabhaxovel <saurabh.php007@gmail.com>
2018-12-13 15:45:22 +0530

    moved some function to common js file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,8 +20,11 @@
     
 @endsection
 
+
 @section('content')
-	@include('includes.headertemplate')
+
+@include('includes.headertemplate')
+
 	<div class="cat-btn">
 		<div class="ui-grid-a top-btn">
 			<div class="ui-block-a"><a href="#" class="ui-btn ui-shadow small-con-30 ui-corner-all icon-eat-active" class="active"><img src="{{asset('images/icons/icon-eat-now-active.png')}}" class="active"><img src="{{asset('images/icons/icon-eat-now-inactive.png')}}" class="inactive">{{ __('messages.Eat Now') }}</a></div>
@@ -96,7 +99,7 @@
 	?>
 	<script type="text/javascript">
 			
-	setLngLat("{{Session::get('with_out_login_lat')}}","{{Session::get('with_out_login_lng')}}");
+	   setLngLat("{{Session::get('with_out_login_lat')}}","{{Session::get('with_out_login_lng')}}");
 
 	</script>
 			<?php
@@ -120,79 +123,9 @@
 
 		getPos("{{url('lat-long')}}","{{url('restro-menu-list/')}}",noImageUrl);
 		checkUserLogin("{{url('checkUserLogin')}}");
-
+		
 	});
 </script>
 
-<script type="text/javascript">
-
-	var count = getCookie("iphonePopupcount") + getCookie("iphonePopupcountIncrease");
-
-	var IphoneVersion;
-    var deviceDetection = function () { 
-    var osVersion, 
-    device, 
-    deviceType, 
-    userAgent, 
-    isSmartphoneOrTablet; 
-
-    device = (navigator.userAgent).match(/Android|iPhone|iPad|iPod/i); 
-
-    if ( /Android/i.test(device) ) { 
-        if ( !/mobile/i.test(navigator.userAgent) ) { 
-            deviceType = 'tablet'; 
-        } else { 
-            deviceType = 'phone'; 
-        } 
-
-        osVersion = (navigator.userAgent).match(/Android\s+([\d\.]+)/i); 
-        osVersion = osVersion[0]; 
-        osVersion = osVersion.replace('Android ', ''); 
-
-    } else if ( /iPhone/i.test(device) ) { 
-        deviceType = 'phone'; 
-        osVersion = (navigator.userAgent).match(/OS\s+([\d\_]+)/i); 
-        osVersion = osVersion[0]; 
-        osVersion = osVersion.replace(/_/g, '.'); 
-        osVersion = osVersion.replace('OS ', ''); 
-
-		document.cookie="osVersion=" + osVersion;		
-
-	    if(osVersion >= 10){
-	    	$('.footer').css({'padding-top':'10px', 'padding-bottom':'10px'});
-	    }			
-
-    } else if ( /iPad/i.test(device) ) { 
-        deviceType = 'tablet'; 
-        osVersion = (navigator.userAgent).match(/OS\s+([\d\_]+)/i); 
-        osVersion = osVersion[0]; 
-        osVersion = osVersion.replace(/_/g, '.'); 
-        osVersion = osVersion.replace('OS ', ''); 
-    } 
-    isSmartphoneOrTablet = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent); 
-    userAgent = navigator.userAgent; 
-    IphoneVersion = osVersion;
-    return { 'isSmartphoneOrTablet': isSmartphoneOrTablet, 
-             'device': device, 
-             'osVersion': osVersion, 
-             'userAgent': userAgent, 
-             'deviceType': deviceType 
-            }; 
-    }();
-    //console.log('IphoneVersion='+IphoneVersion);
-
-	if(getCookie("browser") == 'Safari' && count == 1){
-		document.cookie="iphonePopupcountIncrease=" + 2;
-		var ath = addToHomescreen({
-		    debug: 'ios',           // activate debug mode in ios emulation
-		    skipFirstVisit: false,	// show at first access
-		    startDelay: 0,          // display the message right away
-		    lifespan: 0,            // do not automatically kill the call out
-		    displayPace: 0,         // do not obey the display pace
-		    privateModeOverride: true,	// show the message in private mode
-		    maxDisplayCount: 0      // do not obey the max display count
-		});
-	}
-</script>
-
 @endsection
+
