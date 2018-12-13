4d6686c31e78c3e006e8b45455f64fba01415f1e
saurabhaxovel <saurabh.php007@gmail.com>
2018-12-13 16:40:31 +0530

    test commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 @section('head-scripts')
-
+    
 	<script src="{{asset('js/restolist/fingerprint2.min.js')}}"></script>
     <script src="{{asset('notifactionJs/App42-all-3.1.min.js')}}"></script>
     <script src="{{asset('notifactionJs/SiteTwo.js')}}"></script>
@@ -123,9 +123,81 @@
 
 		getPos("{{url('lat-long')}}","{{url('restro-menu-list/')}}",noImageUrl);
 		checkUserLogin("{{url('checkUserLogin')}}");
-		
+
 	});
+
+	// adding code
+     
+     var count = getCookie("iphonePopupcount") + getCookie("iphonePopupcountIncrease");
+	  var IphoneVersion;
+    var deviceDetection = function () { 
+    var osVersion, 
+    device, 
+    deviceType, 
+    userAgent, 
+    isSmartphoneOrTablet; 
+
+    device = (navigator.userAgent).match(/Android|iPhone|iPad|iPod/i); 
+
+    if ( /Android/i.test(device) ) { 
+        if ( !/mobile/i.test(navigator.userAgent) ) { 
+            deviceType = 'tablet'; 
+        } else { 
+            deviceType = 'phone'; 
+        } 
+
+        osVersion = (navigator.userAgent).match(/Android\s+([\d\.]+)/i); 
+        osVersion = osVersion[0]; 
+        osVersion = osVersion.replace('Android ', ''); 
+
+    } else if ( /iPhone/i.test(device) ) { 
+        deviceType = 'phone'; 
+        osVersion = (navigator.userAgent).match(/OS\s+([\d\_]+)/i); 
+        osVersion = osVersion[0]; 
+        osVersion = osVersion.replace(/_/g, '.'); 
+        osVersion = osVersion.replace('OS ', ''); 
+
+		document.cookie="osVersion=" + osVersion;		
+
+	    if(osVersion >= 10){
+	    	$('.footer').css({'padding-top':'10px', 'padding-bottom':'10px'});
+	    }			
+
+    } else if ( /iPad/i.test(device) ) { 
+        deviceType = 'tablet'; 
+        osVersion = (navigator.userAgent).match(/OS\s+([\d\_]+)/i); 
+        osVersion = osVersion[0]; 
+        osVersion = osVersion.replace(/_/g, '.'); 
+        osVersion = osVersion.replace('OS ', ''); 
+    } 
+    isSmartphoneOrTablet = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent); 
+    userAgent = navigator.userAgent; 
+    IphoneVersion = osVersion;
+    return { 'isSmartphoneOrTablet': isSmartphoneOrTablet, 
+             'device': device, 
+             'osVersion': osVersion, 
+             'userAgent': userAgent, 
+             'deviceType': deviceType 
+            }; 
+    }();
+    //console.log('IphoneVersion='+IphoneVersion);
+
+	if(getCookie("browser") == 'Safari' && count == 1){
+		document.cookie="iphonePopupcountIncrease=" + 2;
+		var ath = addToHomescreen({
+		    debug: 'ios',           // activate debug mode in ios emulation
+		    skipFirstVisit: false,	// show at first access
+		    startDelay: 0,          // display the message right away
+		    lifespan: 0,            // do not automatically kill the call out
+		    displayPace: 0,         // do not obey the display pace
+		    privateModeOverride: true,	// show the message in private mode
+		    maxDisplayCount: 0      // do not obey the max display count
+		});
+	}
+
+	// end of code
 </script>
+
 
 @endsection
 
