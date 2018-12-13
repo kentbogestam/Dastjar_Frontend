cc60a51c62d36899bc3c5e126bf16d92834eb52c
saurabhaxovel <saurabh.php007@gmail.com>
2018-12-13 19:33:53 +0530

    added code for map header

--- resources/views/includes/headertemplate.blade.php
+++ resources/views/includes/headertemplate.blade.php
@@ -3,7 +3,10 @@
     $flag='false';
     $menuActivate='false';
     $selectEatLaterTime='false';
-    $actual_link = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $map='false';
+    $storeMap=false;
+    //$actual_link = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     $baseurl=$app->make('url')->to('/')."/";
     if (strpos($_SERVER['REQUEST_URI'], 'eat-now') !== false || strpos($_SERVER['REQUEST_URI'], 'eat-later') !== false || $actual_link === $baseurl || strpos($_SERVER['REQUEST_URI'], 'home') !== false ) {
 
@@ -20,10 +23,19 @@
        $selectEatLaterTime='true';
       
     }
+     elseif(strpos($_SERVER['REQUEST_URI'], 'search-map-eatnow') !== false || strpos($_SERVER['REQUEST_URI'], 'search-store-map') !== false ){
+       
+       $map='true';
+       if(strpos($_SERVER['REQUEST_URI'], 'search-store-map') !== false){
+          
+          $storeMap='true';
+       }
+      
+    }
  }}
  @endphp
 
- @if($flag=='true'):
+ @if($flag=='true')
 	<div data-role="header" class="header" id="nav-header"  data-position="fixed">
 			<div class="nav_fixed">
 				<div class="logo">
@@ -36,7 +48,8 @@
 				<a href="{{url('search-map-eatnow')}}" class="ui-btn-right map-btn user-link" data-ajax="false"><img src="{{asset('images/icons/map-icon.png')}}" width="30px"></a>
 			</div>
 		</div>
- @elseif($menuActivate=='true'):
+
+ @elseif($menuActivate=='true')
 
  <div data-role="header" class="header"  data-position="fixed" data-tap-toggle="false">
 		<div class="logo">
@@ -48,7 +61,8 @@
 		</div>
 		<a class="ui-btn-right map-btn user-link" href="{{url('search-store-map')}}" data-ajax="false"><img src="{{asset('images/icons/map-icon.png')}}" width="30px"></a>
 	</div>
- @elseif($selectEatLaterTime=='true'):
+
+ @elseif($selectEatLaterTime=='true')
 
  	<div data-role="header" class="header" data-position="fixed" id="nav-header"  data-position="fixed" data-tap-toggle="false"> 
 			<div class="nav_fixed">
@@ -64,4 +78,28 @@
 				href="{{url('search-map-eatnow')}}" data-ajax="false"><img src="{{asset('images/icons/map-icon.png')}}" width="30px"></a>
 			</div>
 		</div>
+
+ @elseif($map=='true')
+ <div data-role="header" class="header" id="nav-header"  data-position="fixed" >
+ 	<div class="nav_fixed">
+          
+          @if($storeMap=='true')
+          <a href="{{url('restro-menu-list/'.$storedetails->store_id)}}" data-ajax="false" class="ui-btn-left text-left backarrow-btn"><img src="{{asset('images/icons/backarrow.png')}}" width="11px"></a>
+
+           
+           @else
+
+			<a href="{{Session::get('route_url')}}" data-ajax="false" class="ui-btn-left text-left backarrow-btn"><img src="{{asset('images/icons/backarrow.png')}}" width="11px"></a>
+		  @endif
+
+		<div class="logo">
+			<div class="inner-logo">
+				<!-- <span class="rest-title">Domino's</span> -->
+				<img src="{{asset('images/logo.png')}}">
+				@if(Auth::check())<span>{{ Auth::user()->name}}</span>@endif
+			</div>
+		</div>
+		<a class="ui-btn-right map-btn user-link" href="#left-side-bar"><img src="{{asset('images/icons/map-icon.png')}}" width="30px"></a>
+	</div>
+	</div>
 @endif
