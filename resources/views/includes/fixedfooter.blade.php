c98b75335c78a56bbc7040396b336b472acace43
Ajit Sigh <ajit.singh.ampliedtech.com>
2019-01-22 18:02:29 +0530

    User: navigation bar in footer make active current selection

--- resources/views/includes/fixedfooter.blade.php
+++ resources/views/includes/fixedfooter.blade.php
@@ -22,42 +22,35 @@
  @endphp
 
 <div data-role="footer" id="footer" data-position="fixed">
-<div class="ui-grid-c inner-footer center">
-	
-		<div class="ui-block-a">
+	<div class="ui-grid-c inner-footer center">
+		<div class="ui-block-a<?php echo ($flag == 'true' || $cart=='true') ? ' active' : ''; ?>">
+	        @if($flag=='true')
+				<a href="javascript:void(0)" class="ui-shadow ui-btn ui-corner-all icon-img ui-btn-inline">
+			@else
+	             @if($cart=='true')
+	             	<a href="#" class="ui-shadow ui-btn ui-corner-all icon-img ui-btn-inline" data-ajax="false" onclick="deleteFullCart('{{ url("emptyCart/") }}','1','{{ __("messages.Leave Cart Page") }}')"> 
+	             @else
+			     	<a href="{{Session::get('route_url')}}" class="ui-shadow ui-btn ui-corner-all icon-img ui-btn-inline" data-ajax="false">
+	            @endif
+			@endif
+				<div class="img-container">
+					<img src="{{asset('images/icons/select-store_01.png')}}">
+				</div>
+				<!-- <span>{{ __('messages.Restaurant') }}</span> -->
+			</a>
+	  	</div>
 
-         @if($flag=='true')
-			<a href="javascript:void(0)" class="ui-shadow ui-btn ui-corner-all icon-img ui-btn-inline">
-		@else
-
-             @if($cart=='true')
-             <a href="#" class="ui-shadow ui-btn ui-corner-all icon-img ui-btn-inline" data-ajax="false" onclick="deleteFullCart('{{ url("emptyCart/") }}','1','{{ __("messages.Leave Cart Page") }}')"> 
-             @else
-		     <a href="{{Session::get('route_url')}}" class="ui-shadow ui-btn ui-corner-all icon-img ui-btn-inline" data-ajax="false">
-            
-            @endif
-            
-		
-		@endif
-			<div class="img-container">
-				<img src="{{asset('images/icons/select-store_01.png')}}">
+        @if($menuActivate=='false')
+			<div class="ui-block-b<?php echo ($cart=='true') ? ' active' : ''; ?>">
+				<a class="ui-shadow ui-btn ui-corner-all icon-img ui-btn-inline">
+					<div class="img-container">
+						<img src="{{asset('images/icon-cart.png')}}">
+					</div>
+					<!-- <span>{{ __('messages.Send') }}</span> -->
+				</a>
 			</div>
-			<!-- <span>{{ __('messages.Restaurant') }}</span> -->
-		</a>
-	  </div>
-
-         @if($menuActivate=='false')
-
-		<div class="ui-block-b"><a class="ui-shadow ui-btn ui-corner-all icon-img ui-btn-inline">
-			<div class="img-container">
-				<img src="{{asset('images/icon-cart.png')}}">
-			</div>
-			<!-- <span>{{ __('messages.Send') }}</span> -->
-		</a></div>
-      
         @else
-
-		  <div class="ui-block-b">
+			<div class="ui-block-b">
 				<a href="#" class="ui-shadow ui-btn ui-corner-all icon-img ui-btn-inline actibe-icon-img" id="menudataSave" data-ajax="false">
 					<div class="img-container">
 						<img src="{{asset('images/icon-cart.png')}}">
@@ -65,7 +58,6 @@
 					<!-- <input type="button" value="{{ __('messages.Send') }}" id="dataSave"/> -->
 				</a>
 			</div>
-
 		@endif
 
 		@include('orderQuantity')
