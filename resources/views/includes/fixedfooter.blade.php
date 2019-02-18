360417e75f0ae0b69de203043f2230438d92a207
Ajit Sigh <ajit.singh.ampliedtech.com>
2019-02-18 17:20:02 +0530

    User: Designing changes, update footer menu style guide as apple

--- resources/views/includes/fixedfooter.blade.php
+++ resources/views/includes/fixedfooter.blade.php
@@ -22,8 +22,8 @@
  @endphp
 
 <div data-role="footer" id="footer" data-position="fixed">
-	<div class="ui-grid-c inner-footer center">
-		<div class="ui-block-a<?php echo ($flag == 'true' || $cart=='true') ? ' active' : ''; ?>">
+	<div class="ui-grid-c inner-footer center customF">
+		<div class="ui-block-a<?php echo ($flag == 'true') ? ' active' : ''; ?>">
 	        @if($flag=='true')
 				<a href="javascript:void(0)" class="ui-shadow ui-btn ui-corner-all icon-img ui-btn-inline">
 			@else
@@ -33,8 +33,8 @@
 			     	<a href="{{Session::get('route_url')}}" class="ui-shadow ui-btn ui-corner-all icon-img ui-btn-inline" data-ajax="false">
 	            @endif
 			@endif
-				<div class="img-container">
-					<img src="{{asset('images/icons/select-store_01.png')}}">
+				<div class="img-container fa fa-cutlery Resto">
+					<!-- <img src="{{asset('images/icons/select-store_01.png')}}"> -->
 				</div>
 				<!-- <span>{{ __('messages.Restaurant') }}</span> -->
 			</a>
@@ -43,19 +43,19 @@
         @if($menuActivate=='false')
 			<div class="ui-block-b<?php echo ($cart=='true') ? ' active' : ''; ?>">
 				<a class="ui-shadow ui-corner-all icon-img ui-btn-inline">
-					<div class="img-container">
-						<span class="badge cart-badge hidden">0</span>
-						<img src="{{asset('images/icon-cart.png')}}">
+					<div class="img-container fa fa-shopping-basket Scart">
+						<span class="badge cart-badge sCartBage hidden">0</span>
+						<!-- <img src="{{asset('images/icon-cart.png')}}"> -->
 					</div>
 					<!-- <span>{{ __('messages.Send') }}</span> -->
 				</a>
 			</div>
         @else
-			<div class="ui-block-b">
+			<div class="ui-block-b <?php echo ($menuActivate=='true') ? ' active' : ''; ?>">
 				<a href="#" class="ui-shadow ui-corner-all icon-img ui-btn-inline actibe-icon-img" id="menudataSave" data-ajax="false">
-					<div class="img-container">
-						<span class="badge cart-badge hidden">0</span>
-						<img src="{{asset('images/icon-cart.png')}}">
+					<div class="img-container fa fa-shopping-basket Scart">
+						<span class="badge cart-badge sCartBage hidden">0</span>
+						<!-- <img src="{{asset('images/icon-cart.png')}}"> -->
 					</div>
 					<!-- <input type="button" value="{{ __('messages.Send') }}" id="dataSave"/> -->
 				</a>
@@ -66,10 +66,10 @@
 
 		<div class="ui-block-d">
 			<a href = "{{url('user-setting')}}" class="ui-shadow ui-btn ui-corner-all icon-img ui-btn-inline" data-ajax="false">
-				<div class="img-container Q">
+				<div class="img-container Q uSeting">
 					<img src="{{asset('images/icons/select-store_07.png')}}">
 				</div>
 			</a>
 		</div>
-  </div>
+	</div>
 </div>
