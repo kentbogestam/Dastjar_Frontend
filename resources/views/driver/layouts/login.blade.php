6d87fe4752243a9e9a159f3d4faa39830460c806
Ajit Sigh <ajit.singh.ampliedtech.com>
2019-07-29 18:56:40 +0530

    Driver: PWA updated

--- resources/views/driver/layouts/login.blade.php
+++ resources/views/driver/layouts/login.blade.php
@@ -1,12 +1,35 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+	<script>
+		(function(document,navigator,standalone) {
+			// prevents links from apps from oppening in mobile safari
+			// this javascript must be the first script in your <head>
+			if ((standalone in navigator) && navigator[standalone]) {
+				var curnode, location=document.location, stop=/^(a|html)$/i;
+				document.addEventListener('click', function(e) {
+					curnode=e.target;
+					while (!(stop).test(curnode.nodeName)) {
+						curnode=curnode.parentNode;
+					}
+					// Condidions to do this only on links to your own app
+					// if you want all links, use if('href' in curnode) instead.
+
+					if('href' in curnode && (curnode.href.indexOf('http') || ~curnode.href.indexOf(location.host)) && (curnode.href.indexOf('#')==-1)) {
+						e.preventDefault();
+						location.href = curnode.href;
+					}
+				},false);
+			}
+		})(document,window.navigator,'standalone');
+	</script>
 	<title>Anar - Delivery App</title>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- <meta name="mobile-web-app-capable" content="yes" />
-	<meta name="apple-mobile-web-app-capable" content="yes" />  -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<link rel="manifest" href="{{asset('assets/driver/manifest.json')}}">
 
 	<link href="{{ url('assets/css/bootstrap.css') }}" rel="stylesheet">
@@ -18,6 +41,9 @@
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
+	<link rel="apple-touch-icon-precomposed" href="{{asset('addToHomeIphoneImage/icon-152x152.png')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('addToHomeIphoneCss/addtohomescreen.css')}}">
+
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
@@ -27,6 +53,12 @@
 	<script type="text/javascript">
 		var BASE_URL = "{{ url('/') }}";
 		var BASE_URL_DRIVER = "{{ url('driver') }}";
+	</script>
+
+	<script src="{{ url('notifactionJs/serviceWorker.js') }}"></script>
+	<script src="{{asset('addToHomeIphoneJs/addtohomescreen.js')}}"></script>
+	<script>
+		registerSwjs();
 	</script>
 </head>
 <body>
@@ -47,10 +79,5 @@
 	<script src="{{ url('assets/js/bootstrap.min.js') }}"></script>
 
 	@yield('scripts')
-
-	<script src="{{ url('notifactionJs/serviceWorker.js') }}"></script>
-	<script>
-		registerSwjs();
-	</script>
 </body>
 </html>
