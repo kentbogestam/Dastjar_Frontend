dfe12b3de2abfafb845a99ab9a3295f9d665e5ec
Ajit Sigh <ajit.singh.ampliedtech.com>
2019-10-24 19:26:29 +0530

    Store: Set interval value in ENV to check check-store-subscription-plan

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -37,41 +37,7 @@
     			});
     		}
 
-    		setInterval(serverSE, 30000);
-
-			/*if(typeof(EventSource) !== "undefined") {
-				console.log('SSE supported!');
-				var es = new EventSource("{{ url('kitchen/check-store-subscription-plan') }}");
-
-				function closeConnection()
-				{
-					es.close();
-					console.log('close');
-				}
-
-				es.addEventListener('message', function(e) {
-					var data = JSON.parse(e.data);
-					console.log('message');
-					if(data.length)
-					{
-						for(var i = 0; i < data.length; i++)
-						{
-							if( $('#menu-'+data[i]).hasClass('ui-state-disabled') )
-							{
-								$('#menu-'+data[i]).removeClass('ui-state-disabled');
-							}
-						}
-					}
-				}, false);
-
-				es.addEventListener('error', function(e) {
-					console.log('error');
-				}, false);
-			}
-			else
-			{
-				console.log('SSE not supported by browser!');
-			}*/
+    		setInterval(serverSE, {{ env('INTERVAL_CHECK_STORE_SUBS') }});
     	@endif
 	</script>
 </body>
