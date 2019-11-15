b3a028f50f81f60a7255da6a1aa5249e685eb925
Ajit Sigh <ajit.singh.ampliedtech.com>
2019-11-15 14:55:01 +0530

    Store: 'check-store-subscription-plan' is too frequent sometimes

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -19,25 +19,38 @@
     <script src="{{asset('kitchenJs/init.js')}}"></script>
     <script type="text/javascript">
     	@if(Session::has('storeId'))
-    		// Server-Sent Events allow a web page to get updates from a server in x second.
+            // Server-Sent Events allow a web page to get updates from a server in x second.
+            let INTERVAL_CHECK_STORE_SUBS = '{{ env('INTERVAL_CHECK_STORE_SUBS') }}';
+
     		var serverSE = function() {
-    			$.get("{{ url('kitchen/check-store-subscription-plan') }}", function(returnedData) {
-    				// console.log('message');
-    				var data = returnedData['data'];
-    				if(data.length)
-					{
-						for(var i = 0; i < data.length; i++)
-						{
-							if( $('#menu-'+data[i]).hasClass('ui-state-disabled') )
-							{
-								$('#menu-'+data[i]).removeClass('ui-state-disabled');
-							}
-						}
-					}
-    			});
+                $.ajax({
+                    url: '{{ url('kitchen/check-store-subscription-plan') }}',
+                    success: function(returnedData) {
+                        // console.log('message');
+                        var data = returnedData['data'];
+                        if(data.length)
+                        {
+                            for(var i = 0; i < data.length; i++)
+                            {
+                                if( $('#menu-'+data[i]).hasClass('ui-state-disabled') )
+                                {
+                                    $('#menu-'+data[i]).removeClass('ui-state-disabled');
+                                }
+                            }
+                        }
+
+                        setTimeout(serverSE, INTERVAL_CHECK_STORE_SUBS);
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        setTimeout(serverSE, INTERVAL_CHECK_STORE_SUBS);
+                    }
+                });
     		}
 
-    		setInterval(serverSE, {{ env('INTERVAL_CHECK_STORE_SUBS') }});
+            if(INTERVAL_CHECK_STORE_SUBS)
+            {
+                setTimeout(serverSE, INTERVAL_CHECK_STORE_SUBS);
+            }
     	@endif
 	</script>
 </body>
