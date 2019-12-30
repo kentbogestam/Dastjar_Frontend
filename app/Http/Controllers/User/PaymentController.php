8b69daf5bae0af8b2b20b6e785d22bc8811f59cf
Ajit Sigh <ajit.singh.ampliedtech.com>
2019-12-30 19:06:56 +0530

    User: Missing OrderID in Stripe order description sometimes

--- app/Http/Controllers/User/PaymentController.php
+++ app/Http/Controllers/User/PaymentController.php
@@ -69,7 +69,7 @@ class PaymentController extends Controller
 		    			->where('order_details.order_id', $orderId)
 		    			->get();
 		    		
-		    		$description = "Order ID: {$request->orderId}; ";
+		    		$description = "Order ID: {$orderId}; ";
 					foreach($orderDetails as $value)
 					{
 						$description .= $value->product_quality . " " . $value->product_name . ", ";
